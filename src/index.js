2cb73e7fcb473c2ece30b7e37081bc123d895d00
Patrick Logan <jpatricklogan@gmail.com>
2017-05-31 17:06:32 -0600

    Firebase working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase';
 
+// Make sure you swap this out with your Firebase app's config
+const config = {
+  apiKey: "AIzaSyAjB5xxpo_eOVJ7LFoDJUN51TGXyhkq1IQ",
+  authDomain: "behavioral-tracker-app.firebaseapp.com",
+  databaseURL: "https://behavioral-tracker-app.firebaseio.com",
+  storageBucket: "behavioral-tracker-app.appspot.com",
+};
+
+const fb = firebase
+  .initializeApp(config)
+  .database()
+  .ref();
+
+const App = (props) => {
+  console.log('snapshot', props);
+  return (
+    <div>
+      <h1>My Prototype</h1>
+      <p>{JSON.stringify(props)}</p>
+    </div>
+  );
+}
+
+fb.on('value', snapshot => {
+  const store = snapshot.val();
+  ReactDOM.render(
+    <App {...store} />,
+    document.getElementById('root')
+  );
+});
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
