136874dd56b635d544a147300b93fe209d8602df
captrat <jpatricklogan@gmail.comgit config user.name>
2017-06-05 11:50:15 -0600

    readd manifest and index.html

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
 
+  const config = {
+    apiKey: "AIzaSyAjB5xxpo_eOVJ7LFoDJUN51TGXyhkq1IQ",
+    authDomain: "behavioral-tracker-app.firebaseapp.com",
+    databaseURL: "https://behavioral-tracker-app.firebaseio.com",
+    storageBucket: "behavioral-tracker-app.appspot.com"
+  };
 
+  const database = firebase
+    .initializeApp(config)
+    .database()
+    .ref();
 
-  render() {
+  const addBehavior = data => database.child('behaviors').push(data, response => response);
+  const updateBehaviors = (id, data) => database.child(`behaviors/${id}`).update(data, response => response);
+  const actions = {
+    addBehaviors,
+    updateBehaviors
+  };
+
+  // Now we can use our actions in our components
+  const App = props => {
+    console.log('snapshot', props);
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <div className="footer">
-            <a href="https://github.com/CaptRat-tiy/Behavioral-Tracker">https://github.com/CaptRat-tiy/Behavioral-Tracker</a>
-          </div>
+      <div>
+        <h1>My Prototype</h1>
+        <button
+          onClick={() => props.addBehavior({ name: 'Perfect Kid!')}}
+        >
+          Add Stuff
+        </button>
+        <ul>
+          {Object.keys(props.behaviors).map((key, index) => <li key={index}>{props.behaviors[key].name}</li>)}
+        </ul>
       </div>
     );
   }
+
+  database.on('value', snapshot => {
+    const store = snapshot.val();
+    ReactDOM.render(
+      <App
+        {...actions}
+        {...store}
+      />,
+      // document.getElementById('root')
+    )
+  });
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import * as firebase from 'firebase';
 
-// Make sure you swap this out with your Firebase app's config
-const config = {
-  apiKey: "AIzaSyAjB5xxpo_eOVJ7LFoDJUN51TGXyhkq1IQ",
-  authDomain: "behavioral-tracker-app.firebaseapp.com",
-  databaseURL: "https://behavioral-tracker-app.firebaseio.com",
-  storageBucket: "behavioral-tracker-app.appspot.com",
-};
+import App from "./App";
 
-const fb = firebase
-  .initializeApp(config)
-  .database()
-  .ref();
-
-const App = (props) => {
-  console.log('snapshot', props);
-  return (
-    <div>
-      <h1>Parsing into the O-bject!</h1>
-      <div>{JSON.stringify(props.courseID.behaviors)} <br/>  <br/> <p>All my Firebase things: <br /> </p>{JSON.stringify(props)}</div>
-    </div>
-  );
-}
-
-fb.on('value', snapshot => {
-  const store = snapshot.val();
-  ReactDOM.render(
-    <App {...store} />,
-    document.getElementById('root')
-  );
-});
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
