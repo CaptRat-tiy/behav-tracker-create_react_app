0f005f5068501bda1ef8361666605f63cbf9ae17
captrat <jpatricklogan@gmail.comgit config user.name>
2017-06-05 12:25:36 -0600

    Firebase connected

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import * as firebase from 'firebase';
-import registerServiceWorker from './registerServiceWorker';
+
 
 class App extends Component {
 
-  const config = {
-    apiKey: "AIzaSyAjB5xxpo_eOVJ7LFoDJUN51TGXyhkq1IQ",
-    authDomain: "behavioral-tracker-app.firebaseapp.com",
-    databaseURL: "https://behavioral-tracker-app.firebaseio.com",
-    storageBucket: "behavioral-tracker-app.appspot.com"
-  };
 
-  const database = firebase
-    .initializeApp(config)
-    .database()
-    .ref();
 
-  const addBehavior = data => database.child('behaviors').push(data, response => response);
-  const updateBehaviors = (id, data) => database.child(`behaviors/${id}`).update(data, response => response);
-  const actions = {
-    addBehaviors,
-    updateBehaviors
-  };
-
-  // Now we can use our actions in our components
-  const App = props => {
-    console.log('snapshot', props);
     return (
-      <div>
-        <h1>My Prototype</h1>
-        <button
-          onClick={() => props.addBehavior({ name: 'Perfect Kid!')}}
-        >
-          Add Stuff
-        </button>
-        <ul>
-          {Object.keys(props.behaviors).map((key, index) => <li key={index}>{props.behaviors[key].name}</li>)}
-        </ul>
-      </div>
+
     );
   }
 
-  database.on('value', snapshot => {
-    const store = snapshot.val();
-    ReactDOM.render(
-      <App
-        {...actions}
-        {...store}
-      />,
-      // document.getElementById('root')
     )
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as firebase from 'firebase';
+import registerServiceWorker from './registerServiceWorker';
 
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  const config = {
+    apiKey: "AIzaSyAjB5xxpo_eOVJ7LFoDJUN51TGXyhkq1IQ",
+    authDomain: "behavioral-tracker-app.firebaseapp.com",
+    databaseURL: "https://behavioral-tracker-app.firebaseio.com",
+    storageBucket: "behavioral-tracker-app.appspot.com"
+  };
+
+  const database = firebase
+    .initializeApp(config)
+    .database()
+    .ref();
+
+  const addBehavior = data => database.child('behaviors').push(data, response => response);
+  const updateBehaviors = (id, data) => database.child(`behaviors/${id}`).update(data, response => response);
+  const actions = {
+    addBehavior,
+    updateBehaviors
+  };
+
+  // Now we can use our actions in our components
+  const App = props => {
+    console.log('snapshot', props);
+    return (
+      <div>
+        <h1>My Prototype</h1>
+        <button
+          onClick={() => props.addBehavior({ name: 'Perfect Kid!'})}
+        >
+          Add Stuff
+        </button>
+        <ul>
+          {Object.keys(props.behaviors).map((key, index) => <li key={index}>{props.behaviors[key].name}</li>)}
+        </ul>
+      </div>
+    );
+  }
+
+  database.on('value', snapshot => {
+    const store = snapshot.val();
+    ReactDOM.render(
+      <App
+        {...actions}
+        {...store}
+      />,
+      document.getElementById('root')
+    )
+  });
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
