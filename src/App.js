b2b126eba0e56a9b7adac8cb8db3488a4220cf74
Patrick Logan <jpatricklogan@gmail.com>
2017-06-26 16:50:09 -0600

    add new behavior/student text fields

--- src/App.js
+++ src/App.js
@@ -28,19 +28,25 @@ export default class App extends Component {
     render() {
     console.log('snapshot', this.props)
 
+
+
     return (
       <div>
         <h1>My App talking with Firebase</h1>
         <button
-          onClick={() => this.props.addBehavior({ name: 'applies new ideas'})}
-        >
+          onClick={() => this.props.addBehavior({ name: 'applies new ideas'})}>
           Add new "behavior" to Firebase database
         </button>
+        <input type="text" id="behaviorName" placeholder="enter new behavior"/>
+        <br/>
+        <br/>
+
         <button
-          onClick={() => this.props.addStudent({ name: 'test'})}
-        >
+          onClick={() => this.props.addStudent({ name: "Socrates"})}>
           Add new student to Firebase database
         </button>
+        <input type="text" id="studentName" placeholder="enter student name"/>
+
         <ul>
           {
             Object.keys(this.props.behaviors).map((key, index) => {
